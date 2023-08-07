f4a711bd19b0e893bc44a5e43ddb46417451c938
hsiu <mr.hsiuting@gmail.com>
2023-08-06 17:49:28 -0700

    Updates the file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
+const dot = document.querySelector(".dot");
 const equals = document.querySelector(".equals");
 const display = document.querySelector(".display");
 const result = document.querySelector(".result");
-const btns = document.querySelectorAll("button");
+const deleNum = document.querySelector(".delete");
+const clear = document.querySelector(".clear");
+
 let total = 0;
 let num = 0;
 let num2 = 0;
@@ -27,15 +30,15 @@ function divide(num, num2){
 }
 
 function operate(op, num, num2){
-    return op(num, num2);
+    return Math.round(op(num, num2)*1000)/1000;
 }
 
 function populateData(e){
     displayValue += e.target.innerText;
+    displayValue = parseFloat(displayValue);
     result.innerText = displayValue;
-    
     if(["+", "-", "x", "/"].includes(ope)){
-        num2 = Number(displayValue)
+        num2 = parseFloat(displayValue)
         if(ope === "+"){
             total = operate(add, num, num2);
         } else if(ope === "-"){
@@ -48,11 +51,43 @@ function populateData(e){
     } 
   
 }
+deleNum.addEventListener("click", () =>{
+    if(total !== 0 && num !== 0 && num2 !== 0){
+        return
+    }
+    else if(result.innerText.length > 0){
+        result.innerText = result.innerText.substring(0, result.innerText.length - 1)
+        displayValue = result.innerText;
+        if(result.innerText === ""){
+            result.innerText = "0";
+            displayValue = "";
+        }
+    } 
+})
+clear.addEventListener("click",() =>{
+    display.innerText = "";
+    result.innerText = "0"
+    displayValue = "";
+    num = 0;
+    num2 = 0;
+    total = 0;
+})
+dot.addEventListener("click", ()=>{
+    if(result.innerText.includes(".")){
+        return;
+    } else if(result.innerText ==="0"){
+        displayValue += "0."
+    }
+    else{
+        displayValue += "."
+    }
+    result.innerText = displayValue;
+})
 numbers.forEach(number => number.addEventListener("click", (e) => {
     populateData(e);
 }))
 operators.forEach(operator => operator.addEventListener("click", (e) => {
-    num = Number(result.innerText);
+    num = parseFloat(result.innerText);
     if(num !== 0 && num2 !== 0){
         num = total;
         result.innerText = total;
@@ -64,24 +99,28 @@ operators.forEach(operator => operator.addEventListener("click", (e) => {
 
 equals.addEventListener("click", () =>{
     // new value after operator
-    num2= Number(displayValue);
+    if(displayValue === ""){
+        alert("plz, provide a value")
+        return;
+    }else{
+        num2= parseFloat(displayValue);
+    }
+   
     if(ope === "+"){
-        result.innerText = operate(add, num, num2);
+        total = operate(add, num, num2);
     }else if(ope === "-"){
-        result.innerText = operate(subtract, num, num2)
+        total = operate(subtract, num, num2);
     }
     else if(ope === "x"){
-        result.innerText = operate(multiply, num, num2)  
+        total = operate(multiply, num, num2);
     }else{
-        result.innerText = operate(divide, num, num2)
+        total = operate(divide, num, num2);
     }
+
     // it shows the calculation process if the user clicks equal
     display.innerText = `${num} ${ope} ${num2} =`;
-    // num = result.innerText;
-    // displayValue = "";
-    
-})
+    result.innerText = total;
+    num = 0;
+    num2 = 0;
 
-// btns.forEach(btn =>(){
-    
-// })
+})
